ced61af14becc49d2a1782d195fe5c83d8941127
Yubraj Bhandari <yubraj.bhandari.hero@gmail.com>
2021-12-25 19:17:52 +0545

    Setup Completed

---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full h-full bg-deep-blue">
+    <h1>Hii</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      "deep-blue": "#1c4774",
+      blue: "#4075ad",
+      "light-blue": "#9ad0e2",
+      cream: "#f3f1e2",
+      brown: "#d3955e",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
